5d549c45943f1441208681775f5bc09a72b48198
Ben Wilson <wilsonatoryt@gmail.com>
2022-10-20 12:12:17 +0100

    Song class functionality

--- MusicApp/src/MusicApp/Song.java
+++ MusicApp/src/MusicApp/Song.java
@@ -1,4 +1,67 @@
 package MusicApp;
 
+/**
+ * The Song class, which stores information about a song
+ */
 public class Song {
+    private Artist artist;
+    private String name;
+    private int numberOfPlays;
+
+    /**
+     * Creates a new song
+     * @param name The name of the song
+     */
+    public Song(String name) {
+        this.name = name;
+        this.numberOfPlays = 0;
+    }
+
+    /**
+     * Creates a new song
+     * @param name The name of the song
+     * @param numberOfPlays The number of times the song has been played
+     */
+    public Song(String name, int numberOfPlays) {
+        this.name = name;
+        this.numberOfPlays = numberOfPlays;
+    }
+
+    /**
+     * Gets the artist of the song
+     * @return The artist
+     */
+    public Artist getArtist() {
+        return artist;
+    }
+
+    /**
+     * Sets the artist of the song
+     */
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    /**
+     * Gets the name of the song
+     * @return The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the number of times the song has been played
+     * @return The number of times the song has been played
+     */
+    public int getNumberOfPlays() {
+        return numberOfPlays;
+    }
+
+    /**
+     * 'Play' the song, incrementing the number of plays
+     */
+    public void play() {
+        this.numberOfPlays += 1;
+    }
 }
